b720c087dd2b060d6c4e1252425ec064b73f9155
YonThiri <yonthiriaung041998@gmail.com>
2024-01-04 11:17:01 +0630

    Initial Commit

---
+++ BritWord/BritWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  BritWordApp.swift
+//  BritWord
+//
+//  Created by Yon Thiri Aung on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BritWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
